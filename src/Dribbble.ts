0587b376a2d4f0c1a27bc1a68ee6776356eeeb4f
Stefan Mansson <stefan.aotik@gmail.com>
2020-02-09 19:08:41 +0100

    feat: add Shots api

--- src/Dribbble.ts
+++ src/Dribbble.ts
@@ -25,4 +25,43 @@ export default class Dribbble {
             },
         };
     }
+
+    public static get shots() {
+        return {
+            list: () => {
+                return Request.fetch({ url: '/user/shots', method: 'GET' });
+            },
+            get: (id: string) => {
+                return Request.fetch({ url: `/shots/${id}`, method: 'GET' });
+            },
+            create: (body: {
+                image: File;
+                title: string;
+                description?: string;
+                low_profile?: boolean;
+                rebound_source_id?: number;
+                scheduled_for?: number;
+                tags?: string[];
+                team_id?: number;
+            }) => {
+                return Request.fetch({ url: `/shots`, method: 'POST', body });
+            },
+            update: (
+                id: string,
+                body: {
+                    title?: string;
+                    description?: string;
+                    low_profile?: boolean;
+                    scheduled_for?: number;
+                    tags?: string[];
+                    team_id?: number;
+                },
+            ) => {
+                return Request.fetch({ url: `/shots/${id}`, method: 'PUT', body });
+            },
+            delete: (id: string) => {
+                return Request.fetch({ url: `/shots/${id}`, method: 'DELETE' });
+            },
+        };
+    }
 }
