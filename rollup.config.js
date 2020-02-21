eb6bed825fc1b5b60711e131debd1d79eb87b7e4
Stefan Mansson <stefan.aotik@gmail.com>
2020-02-21 07:39:49 +0100

    refactor: run terser on build

--- rollup.config.js
+++ rollup.config.js
@@ -1,31 +1,33 @@
-import typescript from "rollup-plugin-typescript2";
-import commonjs from "rollup-plugin-commonjs";
-import external from "rollup-plugin-peer-deps-external";
-import resolve from "rollup-plugin-node-resolve";
-import pkg from "./package.json";
+import typescript from 'rollup-plugin-typescript2';
+import commonjs from 'rollup-plugin-commonjs';
+import external from 'rollup-plugin-peer-deps-external';
+import resolve from 'rollup-plugin-node-resolve';
+import pkg from './package.json';
+import { terser } from 'rollup-plugin-terser';
 
 export default {
-  input: "src/index.ts",
-  output: [
-    {
-      file: pkg.main,
-      format: "cjs",
-      exports: "named"
-    },
-    {
-      file: pkg.module,
-      format: "es",
-      exports: "named"
-    }
-  ],
-  plugins: [
-    external(),
-    resolve(),
-    typescript({
-      clean: true,
-      rollupCommonJSResolveHack: true,
-      exclude: ["*.d.ts", "**/*.d.ts"]
-    }),
-    commonjs()
-  ]
+    input: 'src/index.ts',
+    output: [
+        {
+            file: pkg.main,
+            format: 'cjs',
+            exports: 'named',
+        },
+        {
+            file: pkg.module,
+            format: 'es',
+            exports: 'named',
+        },
+    ],
+    plugins: [
+        external(),
+        resolve(),
+        typescript({
+            clean: true,
+            rollupCommonJSResolveHack: true,
+            exclude: ['*.d.ts', '**/*.d.ts'],
+        }),
+        commonjs(),
+        terser({ sourcemap: false }),
+    ],
 };
